4ce7d4d3b5893db7d500dca56fe49bc8bf74efc7
camdobbie <zx20438@bristol.ac.uk>
2023-04-21 14:08:49 +0100

    began adding dictionaries for each category

--- temp.py
+++ temp.py
@@ -1,11 +1,24 @@
 import pandas as pd
 
-#load Categories_Ingredients/Canned_foods.csv as a pandas dataframe
-df = pd.read_csv('Categories_Ingredients/Meats_and_seafood.csv')
+cats = ["Baking", "Bread_and_bakery", "Breakfast_foods", "Canned_foods", "Condiments", "Dairy_and_eggs", "Drinks", "Frozen", "Fruits_and_vegetables", "Herbs_and_spices", "Meats_and_seafood", "Other", "Pasta_rice_and_beans", "Snacks"]
 
-print(df)
+def makeDict(category):
+    #load Categories_Ingredients/Canned_foods.csv as a pandas dataframe
+    df = pd.read_csv(f'Categories_Ingredients/{category}.csv')
 
-#create a dictionary with raw_ingredients as the keys and simplified_ingredients as the values
-d = dict(zip(df.raw_ingredients, df.simplified_ingredients))
+    #print(df)
+    print(df.shape)
 
-print(d)
+    #create a dictionary with raw_ingredients as the keys and simplified_ingredients as the values
+    d = dict(zip(df.raw_ingredients, df.simplified_ingredients))
+
+    # create a df of the keys and values
+    df2 = pd.DataFrame(list(d.items()), columns=['raw_ingredients', 'simplified_ingredients'])
+    #print the size of the df
+
+    #save the df as a csv file
+    df2.to_csv(f'Dictionaries/{category}.csv', index=False)
+
+for cat in cats:
+    makeDict(cat)
+
