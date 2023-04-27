9e5e7a5cc2fed3014c42f9fff78c94f224292efc
Benjamin Henderson <b.hen02@icloud.com>
2023-04-27 14:04:03 +0100

    Plotted ingredients and sections information

--- unique_ingredients.py
+++ unique_ingredients.py
@@ -6,7 +6,7 @@ df = pd.read_csv('ingredients_simplified.csv')
 unique_ingredients,counts = np.unique(df['simplified_ingredients'], return_counts = True)
 # order the ingredients by the number of times they appear
 unique_ingredients = unique_ingredients[np.argsort(counts)[::-1]]
-counts = counts[np.argsort(counts)[::-1]]
+ingredients_counts = counts[np.argsort(counts)[::-1]]
 # %%
 # create a dataframe with the ingredients, items and price
 ingredients_df = pd.DataFrame(columns = ['recipe_id', 'section', 'ingredient', 'item', 'price'])
@@ -19,6 +19,8 @@ for i in range(len(unique_ingredients)):
     # get the items and prices
     ids = rows['recipe_id'].values
     section = np.unique(rows['section'].values)
+    # strip square brackets from the section
+    section = section[0].replace('[','').replace(']','')
     items = rows['item'].values
     prices = rows['price'].values
     # loop through the items and prices
@@ -29,10 +31,32 @@ for i in range(len(unique_ingredients)):
         except:
             prices[j] = '£' + str(float(prices[j])/float(rows['quantity'].values[j]))
         # add the ingredient, item and price to the dataframe
-        ingredients_df.loc[len(ingredients_df)] = [str(ids),section , ingredient, items[j], prices[j]]
+        ingredients_df.loc[len(ingredients_df)] = [str(ids), section , ingredient, items[j], prices[j]]
 # sort by ingredient name then price (ascending)
 ingredients_df = ingredients_df.sort_values(by=['ingredient', 'price'])
-ingredients_df = ingredients_df.drop_duplicates('ingredient')
+# only keep the first instance of each ingredient
+ingredients_df = ingredients_df.drop_duplicates(subset=['ingredient'], keep='first')
 # save the dataframe as a csv file
 ingredients_df.to_csv('unique_ingredients.csv', index = False)
+# %%
+# plot the frequency of each ingredient
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10,5))
+plt.bar(unique_ingredients[:20], ingredients_counts[:20])
+plt.xticks(rotation=90)
+plt.xlabel('Ingredient')
+plt.ylabel('Frequency')
+plt.title('Frequency of ingredients')
+plt.show()
+# plot the frequency of each section\
+unique_sections,sections_counts = np.unique(ingredients_df['section'].values, return_counts = True)
+unique_sections = unique_sections[np.argsort(sections_counts)[::-1]]
+sections_counts = sections_counts[np.argsort(sections_counts)[::-1]]
+plt.figure(figsize=(10,5))
+plt.bar(unique_sections, sections_counts)
+plt.xticks(rotation=90)
+plt.xlabel('Section')
+plt.ylabel('Frequency')
+plt.title('Frequency of sections')
+plt.show()
 # %%
